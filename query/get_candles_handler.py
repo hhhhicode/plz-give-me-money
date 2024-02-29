4942f9c4c8bbf0141329846e34fd7059ce7d2b6c
hcode <dshcode@douzone.com>
2024-02-29 13:37:18 +0900

    TEST

--- query/get_candles_handler.py
+++ query/get_candles_handler.py
@@ -2,11 +2,8 @@ import requests
 
 
 def get_candles(market, minutes, count):
-    get_candles_url = ("https://api.upbit.com/v1/candles/minutes/"
-                       + str(minutes)
-                       + "?market="
-                       + market
-                       + "&count="
-                       + str(count))
+    get_candles_url = ("https://api.upbit.com/v1/candles/minutes/" + str(minutes)
+                       + "?market=" + market
+                       + "&count=" + str(count))
     get_candles_headers = {"accept": "application/json"}
     return requests.get(get_candles_url, headers=get_candles_headers).text
