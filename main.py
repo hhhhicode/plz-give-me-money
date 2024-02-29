9fc3666c1b76014ebd1a516a7af32bafd97cf08a
hcode <dshcode@douzone.com>
2024-02-29 15:17:08 +0900

    드래그 확대 막음;

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ st.write(ip_utils.get_internal_ip_address())
 
 st.write(ip_utils.get_external_ip())
 
+# Plotly 그래프의 config 설정을 분리하여 정의
+plotly_config = {
+    "scrollZoom": True,
+    "dragMode": False
+}
+
 if st.button('그래프 보기'):
     df = get_candles_handler.fetch_candle_data()
-    st.plotly_chart(get_graph_handler.plot_candlestick(df), use_container_width=True, config={"scrollZoom": True})
+    st.plotly_chart(get_graph_handler.plot_candlestick(df), plotly_config)
