eb12bc8597fca632f83f24becc85a96155ddd33a
hcode <dshcode@douzone.com>
2024-02-29 12:57:41 +0900

    TEST

--- main.py
+++ main.py
@@ -18,11 +18,17 @@ getCandlesHeaders = {"accept": "application/json"}
 #
 # print(response)
 
+st.title('plz give me money')
+
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))  # Google DNS 서버를 이용
     return s.getsockname()[0]
 
-print(get_ip_address())
+st.write(get_ip_address())
 
-# st.title('plz give me money')
+def get_external_ip():
+    response = requests.get('https://httpbin.org/ip')
+    return response.json()['origin']
+
+st.write(get_external_ip())
