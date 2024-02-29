1abda6b6d9180e9399a472663720cd35f9e52b14
hcode <dshcode@douzone.com>
2024-02-29 15:05:54 +0900

    그래프 변경;

--- query/get_graph_handler.py
+++ query/get_graph_handler.py
@@ -8,5 +8,13 @@ def plot_candlestick(df):
                                          low=df['low_price'],
                                          close=df['trade_price'])])
 
-    fig.update_layout(xaxis_rangeslider_visible=False)  # 레인지 슬라이더 비활성화
+    fig.update_layout(
+        xaxis=dict(
+            tickformat='%Y-%m-%d'
+        ),
+        xaxis_rangeslider_visible=False,  # 레인지 슬라이더 비활성화
+        xaxis_title="시간 (KST)",
+        yaxis_title="가격 (KRW)",
+        yaxis_tickformat=','  # 천 단위 구분자를 사용하여 포맷 설정
+    )
     return fig
