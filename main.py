4a0b290fb69e69902d303cea0be1fc85c657e002
hcode <dshcode@douzone.com>
2024-02-29 12:47:46 +0900

    TEST

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
+import socket
 import requests
 
-st.title('plz give me money')
 
 candlesMinutes = 15
 candlesMarket = "KRW-BTC"
@@ -14,7 +14,15 @@ getCandlesUrl = ("https://api.upbit.com/v1/candles/minutes/"
                  + "&count="
                  + str(candlesCount))
 getCandlesHeaders = {"accept": "application/json"}
-response = requests.get(getCandlesUrl, headers = getCandlesHeaders)
+# response = requests.get(getCandlesUrl, headers=getCandlesHeaders)
+#
+# print(response)
 
-print(response)
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))  # Google DNS 서버를 이용
+    return s.getsockname()[0]
 
+print(get_ip_address())
+
+# st.title('plz give me money')
